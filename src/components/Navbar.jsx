2e64643f63a230bb624d9fdb8f4c1378eb437313
DrChilliPepper <shuchu2004@gmail.com>
2025-10-23 16:16:31 +0530

    Refining links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import { IKImage } from 'imagekitio-react'
 import Image from "./Image"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
     return (
         <div className='w-full h-16 md:h-20 flex items-center justify-between'>
             {/*LOGO*/}
-            <div className='flex items-center gap-4 text-2xl font-bold'>
+            <Link to="/" className='flex items-center gap-4 text-2xl font-bold'>
                 <Image src="logo.png" alt="Blog logo" w={32} h={32} />
                 <span>Shulog</span>
-            </div>
+            </Link>
             {/*Mobile Menu*/}
             <div className='md:hidden'>
                 {/*MOBILE BUTTON*/}
@@ -19,10 +20,10 @@ const Navbar = () => {
                 </div>
                 {/*Mobile link list*/}
                 <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 ${open ? "-right-0" : "-right-[100%]"} transition-all ease-in-out bg-[rgba(68,68,68,1)]`}>
-                    <a href="/">Home</a>
-                    <a href="/">Trending</a>
-                    <a href="/">Most Popular</a>
-                    <a href="/">About</a>
+                    <Link to="/">Home</Link>
+                    <Link to="/">Trending</Link>
+                    <Link to="/">Most Popular</Link>
+                    <Link to="/">About</Link>
                     <a href="">
                         <button className='py-2 px-4 rounded-3xl bg-white text-black'>
                             Login
@@ -32,10 +33,10 @@ const Navbar = () => {
             </div>
             {/*Desltop Menu*/}
             <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
-                <a href="/">Home</a>
-                <a href="/">Trending</a>
-                <a href="/">Most Popular</a>
-                <a href="/">About</a>
+                <Link to="/">Home</Link>
+                <Link to="/">Trending</Link>
+                <Link to="/">Most Popular</Link>
+                <Link to="/">About</Link>
                 <a href="">
                     <button className='py-2 px-4 rounded-3xl bg-white text-black'>
                         Login
