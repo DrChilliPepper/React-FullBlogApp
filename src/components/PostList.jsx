1b594a746b5dcd469f410b47d6dfdd406ee95bb2
DrChilliPepper <shuchu2004@gmail.com>
2025-10-24 20:25:36 +0530

    Adding base postlists component

---
+++ src/components/PostList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostList = () => {
+    return (
+        <div className='flex flex-col gap-12 mb-8'>
+            Postlist
+        </div>
+    )
+}
+
+export default PostList
