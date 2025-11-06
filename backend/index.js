960129511ecceb68ea02bfa0d8c78e202393d6d2
DrChilliPepper <shuchu2004@gmail.com>
2025-11-06 17:14:32 +0530

    Connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from "express"
+import connectDB from "./lib/connectDB.js"
+import userRouter from "./routes/user.route.js"
+import postRouter from "./routes/post.route.js"
+import commentRouter from "./routes/comment.route.js"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express()
 
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
+app.use("/comments", commentRouter)
 
 app.listen(3000, () => {
+    connectDB()
     console.log("Server!")
 })
