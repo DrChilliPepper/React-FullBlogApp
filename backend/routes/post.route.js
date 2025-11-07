7cccebb49c149524d1794e487b96c84816fb8d9c
DrChilliPepper <shuchu2004@gmail.com>
2025-11-07 18:50:14 +0530

    Defined schema

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express"
+import Post from "../models/post.model.js"
 
 const router = express.Router()
 
-router.get("anothertest", (req, res) => {
-    res.status(200).send("It Works!")
+router.get("/", async (req, res) => {
+    const posts = await Post.find()
+    res.status(200).send(posts)
 })
-
 
 export default router
