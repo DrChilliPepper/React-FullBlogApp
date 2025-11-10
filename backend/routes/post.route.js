632879cad02dc00130e561d2500f4329341933bb
DrChilliPepper <shuchu2004@gmail.com>
2025-11-10 18:20:31 +0530

    ngrok, clerk endpoint, docs. Store clerk users in DB done

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { getPosts, getPost } from "../controllers/post.controller.js"
+import { getPosts, getPost, createPost, deletePost } from "../controllers/post.controller.js"
 
 const router = express.Router()
 
 router.get("/", getPosts);
+router.delete("/:id", deletePost)
 router.get("/:slug", getPost);
+router.post("/", createPost);
 
 export default router
