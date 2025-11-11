bf07b88a054e6ba56827e871066e4284e9b90c83
DrChilliPepper <shuchu2004@gmail.com>
2025-11-11 19:03:33 +0530

    Patching post data with backend (cors, port,etc)

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ import commentRouter from "./routes/comment.route.js"
 import webhookRouter from "./routes/webhook.route.js"
 import dotenv from "dotenv";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
+import cors from "cors"
 
 dotenv.config();
 
 const app = express()
+app.use(cors(process.env.CLIENT_URL))
 app.use(clerkMiddleware())
 app.use("/webhooks", webhookRouter)
 app.use(express.json())
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,6 +12,12 @@ import LoginPage from "./routes/LoginPage.jsx"
 import MainLayout from './layout/MainLayout.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { dark } from '@clerk/themes'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -55,7 +61,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}
       appearance={{ baseTheme: dark }}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
 )
