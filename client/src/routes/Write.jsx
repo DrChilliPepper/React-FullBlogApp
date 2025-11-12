4b1327cf9aa34b42f3cc721b4cc5f3b802e7bfae
DrChilliPepper <shuchu2004@gmail.com>
2025-11-12 16:42:28 +0530

    Get post input data from client

--- client/src/routes/Write.jsx
+++ client/src/routes/Write.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { useUser } from '@clerk/clerk-react';
+import React, { useState } from 'react'
+import { useAuth, useUser } from '@clerk/clerk-react';
 import 'react-quill-new/dist/quill.snow.css';
 import ReactQuill from 'react-quill-new';
+import { useMutation } from "@tanstack/react-query"
+import axios from "axios";
 
 const Write = () => {
 
     const { isLoaded, isSignedIn } = useUser()
+    const [value, setValue] = useState('');
+    const { getToken } = useAuth();
+
+    const mutation = useMutation({
+        mutationFn: async (newPost) => {
+            const token = await getToken();
+            return axios.post('/posts', newPost, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+        },
+    })
 
     if (!isLoaded) {
         return <div className=''>Loading...</div>
@@ -14,19 +29,33 @@ const Write = () => {
         return <div className=''>You should login first!</div>
     }
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+
+        const data = {
+            title: formData.get("title"),
+            category: formData.get("category"),
+            desc: formData.get("desc"),
+            content: value,
+        }
+
+        console.log(data)
+    }
+
     return (
         <div className='h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6'>
             <h1 className='text-cl font-light'>
                 Create a New Post
             </h1>
-            <form className='flex flex-col gap-6 flex-1 mb-6'>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-6 flex-1 mb-6'>
                 <button className='w-max p-2 shadow-md rounded-xl text-sm text-white bg-[rgba(53,53,53,1)]'>
                     Add a cover image
                 </button>
-                <input className='text-4xl font-semibold bg-transparent outline-none' type="text" placeholder='My Cringe Story' />
+                <input className='text-4xl font-semibold bg-transparent outline-none' type="text" placeholder='My Cringe Story' name='title' />
                 <div className='flex items-center gap-4'>
                     <label htmlFor="" className='text-sm'>Choose a category:</label>
-                    <select name="cat" id="" className='p-2 rounded-xl bg-[rgba(53,53,53,1)] shadow-md'>
+                    <select name="category" id="" className='p-2 rounded-xl bg-[rgba(53,53,53,1)] shadow-md'>
                         <option value="general">General</option>
                         <option value="web-design">Web Design</option>
                         <option value="development">Development</option>
@@ -36,7 +65,7 @@ const Write = () => {
                     </select>
                 </div>
                 <textarea className='p-2 rounded-xl bg-[rgba(53,53,53,1)] shadow-md' name="desc" placeholder='A short note' />
-                <ReactQuill theme="snow" className='flex-1 rounded-xl bg-[rgba(53,53,53,1)] shadow-md text-white' />
+                <ReactQuill theme="snow" className='flex-1 rounded-xl bg-[rgba(53,53,53,1)] shadow-md text-white' value={value} onChange={setValue} />
                 <button className='bg-[rgba(255,119,119,1)] text-white font-medium rounded-xl mt-4 p-2 w-36'>
                     Send
                 </button>
